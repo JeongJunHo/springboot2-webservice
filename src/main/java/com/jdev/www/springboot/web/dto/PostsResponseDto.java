2b55ea2175c68196c20c70d67e6fd6b9086df096
JeongJunHo <jjh49470826@gmail.com>
2020-01-07 23:42:11 +0900

    main 컨텐츠 추가

---
+++ src/main/java/com/jdev/www/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.jdev.www.springboot.web.dto;
+
+import com.jdev.www.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
