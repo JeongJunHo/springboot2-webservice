5a7e8d26ec2882ef69188bad1e72efd8ecc9e304
JeongJunHo <jjh49470826@gmail.com>
2020-01-11 23:02:10 +0900

    Test환경수정 및 소셜 로그인 추가

--- src/main/java/com/jdev/www/springboot/web/IndexController.java
+++ src/main/java/com/jdev/www/springboot/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.jdev.www.springboot.web;
 
+import com.jdev.www.springboot.config.auth.LoginUser;
+import com.jdev.www.springboot.config.auth.dto.SessionUser;
+import com.jdev.www.springboot.domain.user.User;
 import com.jdev.www.springboot.service.posts.PostsService;
 import com.jdev.www.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -10,14 +13,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
